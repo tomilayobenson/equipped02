9f71a9379270aa43452ae72f748bb6f133a144f4
Karthik <viswanadhasashi@gmail.com>
2022-12-30 19:38:46 -0500

    prodcutslistings added

---
+++ src/pages/PurchaseListings.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container } from 'reactstrap'
+import ProductList from '../components/Products/ProductList'
+
+const PurchaseListings = () => {
+  return (
+        <Container>
+     <ProductList />
+     </Container>
+  )
+}
+
+export default PurchaseListings
