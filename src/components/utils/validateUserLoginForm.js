a9a2e94baa80d01af50306f9e74912b5a3f9c297
Fernando <fernando82628@yahoo.com>
2023-01-05 05:58:19 -0600

    Added utils component
    utils and added code to login form

---
+++ src/components/utils/validateUserLoginForm.js
@@ -0,0 +1,18 @@
+export const validateUserLoginForm = (values) => {
+    const {username ,password} = values
+      const errors = {}
+
+      if(!username){
+          errors.username = 'Required'
+      }else if(username.length < 6){
+          errors.username ='Must be atleast 6 characters'
+      }else if(username.length >15){
+          errors.username ='Must be 15 characters or less'
+      }
+       if(!password){
+           errors.password ='Required'
+       }else if(password.length < 8){
+           errors.password ='Must be atleast 8 characters'
+       } 
+       return errors;
+}
